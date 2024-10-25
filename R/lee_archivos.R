110536d5b6fd7efe48e8d370ee53f7ee376923b4
celestedallago <celedallago@gmail.com>
2024-10-25 11:14:51 -0300

    agrego funcion para leer archivos

---
+++ R/lee_archivos.R
@@ -0,0 +1,32 @@
+
+#' Leer archivos
+#'
+#'
+#' Lo que hace esta funcion es leer tu archivo,
+#' puede ser tanto URL como rutas locales
+#'
+#'
+#' @param ruta
+#'
+#'
+#' @return
+#' Te devuelve el archivo leido
+#'
+#' @examples
+#'
+#' @export
+#'
+#'
+leer_archivo <- function(ruta) {
+  # Detectar la extensión del archivo
+  extension <- tools::file_ext(ruta)
+
+  # Leer archivo según la extensión
+  switch(tolower(extension),
+         "csv" = read.csv(ruta, stringsAsFactors = FALSE),
+         "txt" = read.delim(ruta, stringsAsFactors = FALSE),
+         "xlsx" = openxlsx::read.xlsx(ruta),
+         "rds" = readRDS(ruta),
+         stop("Formato no soportado: ", extension)
+  )
+}
