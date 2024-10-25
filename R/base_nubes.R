62944cc4e588a6c757ca38eb4d826471c1743b37
VictoriaMarull <vmarull@mail.austral.edu.ar>
2024-10-25 10:49:56 -0300

    Nueva funcion

--- R/base_nubes.R
+++ R/base_nubes.R
@@ -1,7 +1,22 @@
-
-min_max_por_estacion <- function(datos) {
+#' Minimos y maximos por por fecha
+#'
+#'
+#'La funcion 'min_max_por_estacion()' te devuelve las temperaturas minimas y maximas registradas en cada mes
+#'
+#' Esta es informacion extra
+#'
+#' @return
+#' Poner la estacion que deseamos ver
+#' Ejemplo:
+#'
+#'
+#' @examples
+#' min_max_por_estacion()
+#' @export
+min_max_por_mes <- function(datos) {
   datos %>%
-    group_by(estacion) %>%
+    mutate(mes = format(as.Date(fecha), "%Y-%m")) %>%  # Extrae el año y mes de la fecha
+    group_by(mes) %>%
     summarise(
       temp_min = min(temperatura, na.rm = TRUE),
       temp_max = max(temperatura, na.rm = TRUE)
